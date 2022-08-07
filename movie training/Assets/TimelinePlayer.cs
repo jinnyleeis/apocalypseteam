dd2fac90f36f129909b8bc9c5d16c763fc30c4f6
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2022-08-07 17:10:49 +0900

    .

--- movie training/Assets/TimelinePlayer.cs
+++ movie training/Assets/TimelinePlayer.cs
@@ -5,8 +5,16 @@ using UnityEngine.Playables;
 
 public class TimelinePlayer : MonoBehaviour
 {
-    private PlayableDirector director;
+    private PlayableDirector director;//나중에 쓸 것임
     public GameObject controlPanel;
+    
+
+
+
+    void Start() {
+        gameObject.SetActive(false);
+        
+    }
 
     private void Awake()
     {
@@ -17,16 +25,20 @@ public class TimelinePlayer : MonoBehaviour
 
     private void Director_Stopped(PlayableDirector obj)
     {
+        Debug.Log("ended ");
         controlPanel.SetActive(true);
+        Debug.Log("ended ");
     }
 
     private void Director_Played(PlayableDirector obj)
     {
+        Debug.Log("started ");
         controlPanel.SetActive(false);
     }
 
     public void StartTimeline()
     {
+         gameObject.SetActive(true);
         director.Play();
-    }
+   }
 }
